01f67fe715e1dad93d6b62ceb958d31b402ec2f9
edocai <edocaai@hotmail.com>
2022-07-11 19:21:42 +0100

    contains duplicate

---
+++ 217. Contains Duplicate/containsDuplicate.js
@@ -0,0 +1,11 @@
+const containsDuplicate = nums => {
+    var obj = {};
+
+    for (var i = 0; i < nums.length; i++) {
+        obj[nums[i]] = obj[nums[i]] + 1 || 1;
+
+        if (obj[nums[i]] > 1) return true;
+    }
+
+    return false;
+};
