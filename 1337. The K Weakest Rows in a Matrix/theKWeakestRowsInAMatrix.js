779bb5ff003a1fb62ae486e0d6333d434f49184a
edocai <edocaai@hotmail.com>
2022-07-17 15:01:04 +0100

    the K weakest rows in a matrix

---
+++ 1337. The K Weakest Rows in a Matrix/theKWeakestRowsInAMatrix.js
@@ -0,0 +1,14 @@
+const kWeakestRows = (mat, k) => {
+    let objArray = []
+    for (let i = 0; i < mat.length; i++) {
+        objArray.push({
+            index: i,
+            count: mat[i].filter(i => i == 1).length
+        })
+    }
+    let newObj = objArray.sort((a, b) => a.count - b.count).slice(0, k)
+    finalArray = []
+    for (o of newObj)
+        finalArray.push(o.index)
+    return finalArray
+};
