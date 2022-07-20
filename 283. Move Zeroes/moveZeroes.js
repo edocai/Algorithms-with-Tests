adce2894f9ed6421c4f3bccebb37858c9eb16ac8
edocai <edocaai@hotmail.com>
2022-07-20 19:12:42 +0100

    move zeroes

---
+++ 283. Move Zeroes/moveZeroes.js
@@ -0,0 +1,16 @@
+const moveZeroes = nums => {
+    // two pointer method - one iterates other focuses on non-zero elems
+    let count = 0
+    let len = nums.length
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[count++] = nums[i]
+        }
+    }
+    // after checking non-zero elems - starts from count and just adds 0's
+    // to the end of array length
+    for (let i = count; i < len; i++) {
+        nums[i] = 0
+    }
+    return nums
+};
