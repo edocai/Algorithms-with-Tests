b8d66748e2ffae0e869d3e1540f2ced1f0d510a7
edocai <edocaai@hotmail.com>
2022-07-22 14:46:49 +0100

    contains duplicate test case

--- 217. Contains Duplicate/containsDuplicate.test.js
+++ 217. Contains Duplicate/containsDuplicate.test.js
@@ -1,9 +1,9 @@
 const containsDuplicate = require('./containsDuplicate')
 
 test('should return true or false if there is a duplicate or not', () => {
-    expect(containsDuplicate([1, 2, 3, 1])).toEqual(true)
+    expect(containsDuplicate([1, 2, 3, 1])).toBeTruthy()
 })
 
 test('should return true or false if there is a duplicate or not', () => {
-    expect(containsDuplicate([1, 2, 3])).toEqual(false)
+    expect(containsDuplicate([1, 2, 3])).toBeFalsy()
 })
