de4c9d33c50f8d2e05c891951057599aa8db8cc9
edocai <edocaai@hotmail.com>
2022-07-28 17:37:21 +0100

    majority element

---
+++ 169. Majority Element/majorityElement.js
@@ -0,0 +1,8 @@
+const majorityElement = nums => {
+    let count = {}; // mapping of element to it's frequency
+    for (n of nums) {
+        count[n] ? count[n]++ : count[n] = 1;
+        if (count[n] > nums.length / 2) return n;
+    }
+    return -1;
+};
