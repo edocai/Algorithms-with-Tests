09c5514b8c9d97e4d28fe6404d6965f1081e8ef8
edocai <edocaai@hotmail.com>
2023-01-05 17:32:19 +0300

    pseudocode

---
+++ 876. Middle of the Linked List/middleOfLinkedList.js
@@ -0,0 +1,12 @@
+const middleNode = head => {
+
+};
+
+//Fast & slow pointer method
+//loop through the list while fast && fast.next is not null
+    //increment the fast pointer 2 times and slow once
+//when fast or fast.next === null break out and return slow
+//at that time slow will be in the middle of the list
+
+//Time Complexity: O(n)
+//Space Complexity: O(1)
