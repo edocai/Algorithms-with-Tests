7ce2192a20d6bfe7c3a0a2c9342c0a61facd18e1
edocai <edocaai@hotmail.com>
2023-01-06 22:20:16 +0300

    implementation

---
+++ 20. Valid Parentheses/validParenthesis.js
@@ -0,0 +1,20 @@
+const isValid = s => {
+    const hashMap = { "(": ")", "{": "}", "[": "]" };
+    const stack = [];
+    for (let ch of s) {
+        if (hashMap[ch]) {
+            // ch is an opening bracket
+            stack.push(hashMap[ch]);
+        } else if (stack.length > 0 && stack[stack.length - 1] === ch) {
+            // ch is a closing bracket and top of stack matches
+            stack.pop();
+        } else {
+            // ch is a closing bracket and top of the stack doesn't match
+            return false;
+        }
+    }
+    return stack.length === 0;
+};
+
+//Time: O(n)
+//Space: O(n)
