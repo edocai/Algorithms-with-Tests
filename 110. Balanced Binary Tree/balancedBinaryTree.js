6c04154cb13cb733fcfa22ddbaea21048417ca22
edocai <edocaai@hotmail.com>
2023-01-07 17:54:45 +0300

    implementation

--- 110. Balanced Binary Tree/balancedBinaryTree.js
+++ 110. Balanced Binary Tree/balancedBinaryTree.js
@@ -1,5 +1,19 @@
 const isBalanced = root => {
+    if (!root) return true
+    let max = 1
 
+    const dfs = node => {
+        if (!node) return 0;
+
+        let left = dfs(node.left)
+        let right = dfs(node.right)
+        if (Math.abs(left - right) > 1) {
+            max--
+        }
+        return Math.max(left, right) + 1
+    }
+    dfs(root)
+    return max === 1
 };
 
 //DFS recursion
