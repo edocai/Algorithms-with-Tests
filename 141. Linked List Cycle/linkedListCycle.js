ed59a09bfe472c932a2e121944962ed89088288a
edocai <edocaai@hotmail.com>
2023-01-07 17:53:34 +0300

    pseudocode

---
+++ 141. Linked List Cycle/linkedListCycle.js
@@ -0,0 +1,11 @@
+const hasCycle = head => {
+
+};
+
+//fast&slow pointer
+//similar to finding the middle of a linked list
+//simply check if fast === slow to break out of the while loop
+//else return false
+
+//Time: O(n)
+//Space: O(1)
