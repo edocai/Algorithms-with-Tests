877f722cc0146aa12b0c9a4ee13f6119c2339a16
edocai <edocaai@hotmail.com>
2023-01-08 20:45:16 +0300

    pseudocode

---
+++ 1299. Replace Elements with Greatest Element on Right Side/replaceElements.js
@@ -0,0 +1,12 @@
+const replaceElements = arr => {
+
+};
+
+//new array of length arr
+//let end of result elem = -1
+//loop through backwards and check vertically arr[i] and result[i]
+    //result will now equal the max of the two
+//return result
+
+//Time: O(n)
+//Space: O(n)
