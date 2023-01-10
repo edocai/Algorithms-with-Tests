cf3c4ff962e5c1e219f6eb690cdffa6ad6407245
edocai <edocaai@hotmail.com>
2023-01-10 21:18:27 +0300

    pseudocode

---
+++ 392. Is Subsequence/isSubsequence.js
@@ -0,0 +1,14 @@
+const isSubsequence = function (s, t) {
+
+};
+
+//Two pointer method
+//start at each string beginning
+//if the char at both points are equal
+    //increment both
+//else
+    //increment pointer for second string
+//check if first point equals length of s
+
+//Time: O(n)
+//Space: O(1)
