176960736d2e49143ad414a12f7fd58c98a3b1c1
edocai <edocaai@hotmail.com>
2023-01-10 21:18:38 +0300

    implementation

--- 392. Is Subsequence/isSubsequence.js
+++ 392. Is Subsequence/isSubsequence.js
@@ -1,5 +1,16 @@
 const isSubsequence = function (s, t) {
+    let pointA = 0
+    let pointB = 0
 
+    while (pointA < s.length && pointB < t.length) {
+        if (s[pointA] === t[pointB]) {
+            pointA++
+            pointB++
+        } else {
+            pointB++
+        }
+    }
+    return pointA === s.length
 };
 
 //Two pointer method
