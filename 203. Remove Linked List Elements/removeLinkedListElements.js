2e36fde0eb254bcbb666c11fa12f23ce00defbf3
edocai <edocaai@hotmail.com>
2023-01-12 21:07:35 +0300

    pseudocode

---
+++ 203. Remove Linked List Elements/removeLinkedListElements.js
@@ -0,0 +1,15 @@
+const removeElements = (head, val) => {
+
+};
+
+//Initiate dummy node as ListNode(-1) and set it to be the new head: dummy.next = head.
+//Initiate two pointers to track the current node and its predecessor: curr and prev.
+//While curr is not a null pointer:
+//Compare the value of the current node with the value to delete.
+//In the values are equal, delete the current node: prev.next = curr.next.
+//Otherwise, set predecessor to be equal to the current node.
+//Move to the next node: curr = curr.next.
+//Return dummy.next.
+
+//Time: O(n)
+//Space: O(1)
