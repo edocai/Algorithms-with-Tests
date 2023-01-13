b7f1738ff1affc12403de4c593af0f3d83b2a15d
edocai <edocaai@hotmail.com>
2023-01-13 21:48:13 +0300

    pseudocode

---
+++ 1984. Minimum Difference Between Highest and Lowest of K Scores/minDiffOfHighLow.js
@@ -0,0 +1,13 @@
+const minimumDifference = (nums, k) => {
+
+};
+
+//sliding window
+//sort the array
+//while right < left
+    //find min of res(set highest number) and result of subtracting two pointers
+    //increment both right and left
+//return res
+
+//Time: O(nlogn)
+//Space: O(1)
