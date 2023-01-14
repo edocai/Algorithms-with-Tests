d59d1f8fd361d62a7aeee439fc89b3b9cfa25285
edocai <edocaai@hotmail.com>
2023-01-14 20:39:50 +0300

    implementation + comments

---
+++ 1544. Make The String Great/makeTheStringGreat.js
@@ -0,0 +1,19 @@
+const makeGood = s => {
+    let stack = []
+
+    //loop through the string
+    for (let i = 0; i < s.length; i++) {
+        //if minus of charCodeAt from s[i] and end of stack === 32 
+        //then they are the same and you pop 
+        if (stack.length && Math.abs(s[i].charCodeAt() - stack[stack.length - 1].charCodeAt()) === 32) {
+            stack.pop()
+        } else {
+            //else just push the value
+            stack.push(s[i])
+        }
+    }
+    return stack.join('')
+};
+
+//Time: O(n)
+//Space: O(n)
