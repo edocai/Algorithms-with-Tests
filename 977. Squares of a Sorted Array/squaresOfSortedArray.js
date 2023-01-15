55e51bcce6f84ed8f6dd7400083fefc7d9aa14ac
edocai <edocaai@hotmail.com>
2023-01-15 22:02:02 +0300

    implementation of brute force method

---
+++ 977. Squares of a Sorted Array/squaresOfSortedArray.js
@@ -0,0 +1,18 @@
+// const sortedSquares = nums => {
+//     squaredArray = []
+//     for(let i = 0; i < nums.length; i++) {
+//         squaredArray.push(nums[i] * nums[i])
+//     }
+//     return squaredArray.sort((a, b) => a - b)
+// };
+
+//square every element in the array
+//return that array but sorted
+
+//Time Complexity: O(nlogn)
+//Space Complexity: O(n)
+
+const sortedSquares = nums => {
+
+};
+
