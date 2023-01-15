8557d678b4a8db2ff710f7c805a90023b76f8db9
edocai <edocaai@hotmail.com>
2023-01-15 22:02:14 +0300

    pseudocode

--- 977. Squares of a Sorted Array/squaresOfSortedArray.js
+++ 977. Squares of a Sorted Array/squaresOfSortedArray.js
@@ -16,3 +16,15 @@ const sortedSquares = nums => {
 
 };
 
+//Two pointer method
+//Create a results array with nums length
+//Let results index = nums.length - 1
+//while left <= right
+    //just check if nums[left]^2 is bigger than nums[right]^2
+        //results[resIdx] = nums[left]^2
+    //else
+        //results[resIdx] = nums[right]^2
+//return results
+
+//Time Complexity: O(n)
+//Space Complexity: O(n)
