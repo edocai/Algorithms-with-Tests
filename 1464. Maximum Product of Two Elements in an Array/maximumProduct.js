59f26b4df699a4a8b9539ee300406d8bde9bcf6e
edocai <edocaai@hotmail.com>
2023-02-15 17:00:52 +0000

    implementation + comments

---
+++ 1464. Maximum Product of Two Elements in an Array/maximumProduct.js
@@ -0,0 +1,20 @@
+function maxProduct(nums) {
+  // let results = []
+
+  // for(let i = 0; i < nums.length; i++) {
+  //     for(let j = i + 1; j < nums.length; j++) {
+  //         results.push((nums[i] - 1)*(nums[j] - 1))
+  //     }
+  // }
+  // return Math.max(...results)
+
+  //Time: O(n^2)
+  //Space: O(n)
+
+  nums.sort((a, b) => a - b)
+  let i = nums.length - 1
+  return (nums[i] - 1) * (nums[i - 1] - 1)
+
+  //Time: O(nlog(n))
+  //Space: O(1)
+};
