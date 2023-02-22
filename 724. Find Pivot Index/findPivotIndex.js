4199f384348af078c72c77fe30c8a300029ea530
edocai <edocaai@hotmail.com>
2023-02-22 20:05:13 +0000

    pseudocode

---
+++ 724. Find Pivot Index/findPivotIndex.js
@@ -0,0 +1,14 @@
+var pivotIndex = function (nums) {
+
+};
+
+//math
+//first find the total of the array of integers
+//then check until the calculation is equal to leftSum
+    //return i
+//else
+    //continue to add to leftSum
+
+
+//Time: O(n)
+//Space: O(1)
