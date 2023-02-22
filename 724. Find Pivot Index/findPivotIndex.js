1a9f83c6011d95908c1b72021be6281ad7980a82
edocai <edocaai@hotmail.com>
2023-02-22 20:08:13 +0000

    implementation + comments

--- 724. Find Pivot Index/findPivotIndex.js
+++ 724. Find Pivot Index/findPivotIndex.js
@@ -1,5 +1,23 @@
 var pivotIndex = function (nums) {
+  let leftSum = 0
+  let sum = 0
 
+  nums.forEach((num) => {
+    sum += num
+  })
+
+  for (let i = 0; i < nums.length; i++) {
+    //  28 - 0 - 1 === 0
+    //  28 - 1 - 7 === 1
+    //  28 - 8 - 3 === 8
+    //  28 - 11 - 6 === 11
+    if (sum - leftSum - nums[i] === leftSum) {
+      return i
+    } else {
+      leftSum += nums[i]
+    }
+  }
+  return -1
 };
 
 //math
