e73409774a20ee2d6b7490b79185c7b25e17d411
edocai <edocaai@hotmail.com>
2023-02-23 19:08:20 +0000

    pseudocode + naive approach

---
+++ 219. Contains Duplicate II/containsDuplicate.js
@@ -0,0 +1,22 @@
+var containsNearbyDuplicate = function (nums, k) {
+  // for(let i = 0; i < nums.length; i++) {
+  //     for(let j = i + 1; j < nums.length; j++) {
+  //         if(nums[i] === nums[j] && Math.abs(i - j) <= k) {
+  //             return true
+  //         }
+  //     }
+  // }
+  // return false
+  //Time: O(n^2)
+  //Space: O(1)
+
+
+};
+
+//hash map
+//if the num is in map then check the diff of indexes
+    //return true if they are less than or equals to k
+//else place the index of the num in the map
+
+//Time: O(n)
+//Space: O(n)
