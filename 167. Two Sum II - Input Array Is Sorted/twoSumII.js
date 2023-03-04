05012d093c0f87e59b54b3196b2588ae5c4e5f2a
edocai <edocaai@hotmail.com>
2023-03-04 17:21:23 +0000

    pseudocode

---
+++ 167. Two Sum II - Input Array Is Sorted/twoSumII.js
@@ -0,0 +1,14 @@
+const twoSum = (numbers, target) => {
+
+};
+
+//Two pointers
+//left and a right
+//loop while left is smaller
+//sum varianle that sums the two pointers
+    //if sum === target 
+        //return pointers with + 1
+    //else increment each pointer respectively
+
+//Time: O(n)
+//Space: O(1)
