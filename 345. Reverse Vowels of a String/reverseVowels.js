8e186bc231f40e5982035f94a60e90e408a5ee53
edocai <edocaai@hotmail.com>
2023-03-04 17:25:16 +0000

    pseudocode

---
+++ 345. Reverse Vowels of a String/reverseVowels.js
@@ -0,0 +1,17 @@
+var reverseVowels = function (s) {
+
+};
+
+//Two pointer method
+//create map for vowels
+//change string to an array as strings are immutable
+//while left pointer is smaller than right
+    //and char at right is not in vowels
+        //decrement right
+    //and char at left is not in vowels
+        //increment left
+//otherwise just swap left and right and increment/decrement both
+//return array.join to turn it back to a string
+
+//Time: O(n)
+//Space: O(n)
