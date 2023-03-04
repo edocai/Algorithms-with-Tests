00a2c2a75ffb8b0a1a8a92dc7061b70ec8cda12e
edocai <edocaai@hotmail.com>
2023-03-04 17:21:34 +0000

    implementation

--- 167. Two Sum II - Input Array Is Sorted/twoSumII.js
+++ 167. Two Sum II - Input Array Is Sorted/twoSumII.js
@@ -1,5 +1,17 @@
 const twoSum = (numbers, target) => {
+        let left = 0
+        let right = numbers.length - 1
 
+        while(left < right) {
+            let sum = numbers[left] + numbers[right]
+            if(sum === target) {
+                return [left + 1, right + 1]
+            } else if(sum > target) {
+                right--
+            } else {
+                left++
+            }
+        }
 };
 
 //Two pointers
