77e6f893a3afbb0e0c768675923db680b1f6672a
edocai <edocaai@hotmail.com>
2023-03-12 20:26:33 +0000

    implementation

--- 27. Remove Element/removeElement.js
+++ 27. Remove Element/removeElement.js
@@ -1,5 +1,16 @@
 const removeElement = (nums, val) => {
+    let left = 0
+    let right = nums.length - 1
 
+    while(left <= right) {
+        if(nums[left] === val) {
+            nums[left] = nums[right]
+            right--
+        } else {
+            left++
+        }
+    }
+    return left
 };
 
 //Two pointer method
