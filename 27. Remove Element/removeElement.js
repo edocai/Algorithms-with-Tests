53075c61db7b970d9d565f4e6079006b7f27d561
edocai <edocaai@hotmail.com>
2023-03-12 20:26:21 +0000

    pseudocode

---
+++ 27. Remove Element/removeElement.js
@@ -0,0 +1,11 @@
+const removeElement = (nums, val) => {
+
+};
+
+//Two pointer method
+//if nums[left] === val replace with nums[right] and decrement right
+//else increment left
+//return left
+
+//Time Complexity: O(n)
+//Space Complexity: O(1)
